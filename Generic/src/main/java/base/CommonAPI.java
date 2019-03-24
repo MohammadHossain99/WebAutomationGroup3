f1b7f085a919bff4498ffdee8ef3f09b399b9af4
MohammadHossain99 <mshossain1166@gmail.com>
2019-03-24 13:50:53 -0400

    degfdgj

--- Generic/src/main/java/base/CommonAPI.java
+++ Generic/src/main/java/base/CommonAPI.java
@@ -1,53 +1,49 @@
 package base;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
-import java.util.concurrent.TimeUnit;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 public class CommonAPI {
 
 
     public WebDriver driver = null;
 
-    //@Parameters({"url"})
+    @Parameters({"url"})
     @BeforeMethod
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\nehal\\project\\WebAutomationGroup3\\Generic\\browser-driver\\chromedriver.exe");
+    public void setUp(@Optional("https://www.bbc.com/") String url) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\nehal\\project\\WebAutomationGroup3\\Generic\\drivers\\chromedriver.exe");
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
     }
 
-    public void mouseHoverByXpath(String locator) {
-        try {
-            WebElement element = driver.findElement(By.xpath(locator));
-            Actions action = new Actions(driver);
-            action.moveToElement(element).build().perform();
-            //action.moveToElement( driver.findElement( By.xpath( "//div[@class='hl-cat-nav__sub-cat-col']/ul/li[3]" ) ));
-            //Thread.sleep(1500);
-            //action.click().build().perform();
-        } catch (Exception ex) {
-            System.out.println("First attempt has been done, This is second try");
-            WebElement element = driver.findElement(By.cssSelector(locator));
-            Actions action = new Actions(driver);
-            action.moveToElement(element).perform();
-
-
-        }
-
-    }
+//  public void mouseHoverByXpath(String locator) {
+//        try {
+//            WebElement element = driver.findElement(By.xpath(locator));
+//            Actions action = new Actions(driver);
+//            action.moveToElement(element).build().perform();
+//            //action.moveToElement( driver.findElement( By.xpath( "//div[@class='hl-cat-nav__sub-cat-col']/ul/li[3]" ) ));
+//            //Thread.sleep(1500);
+//            //action.click().build().perform();
+//        } catch (Exception ex) {
+//            System.out.println("First attempt has been done, This is second try");
+//            WebElement element = driver.findElement(By.cssSelector(locator));
+//            Actions action = new Actions(driver);
+//            action.moveToElement(element).perform();
+//
+//
+//        }
+//
+//    }
 
 
     @AfterMethod
     public void cleanUp() {
-        //driver.navigate().back();
         driver.close();
     }
 }
--- bbc/src/test/java/test/TestBbcAccount.java
+++ bbc/src/test/java/test/TestBbcAccount.java
@@ -3,7 +3,6 @@ package test;
 import base.CommonAPI;
 import homePage.BbcAccount;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -28,8 +27,8 @@ public class TestBbcAccount extends CommonAPI {
 //        String title1 = driver.getPageSource();
 //        Assert.assertEquals(title1, "https://www.bbc.com/");
            }
-    @AfterMethod
-    public void cleanUp() {
-        driver.close();
-    }
+//    @AfterMethod
+//    public void cleanUp() {
+//        driver.close();
+//    }
 }
